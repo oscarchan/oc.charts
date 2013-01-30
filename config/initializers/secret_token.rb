22da158e8b31b879003ec1d541dc862e8a325d35
Oscar Chan <oscarc@surveymonkey.com>
2013-01-29 23:28:13 -0800

    initial check in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OcCharts::Application.config.secret_token = '0e44ed041bb4c501e67bfd188e02df329cc4accbbad733e6a525bccfd0e9ca0ce69306cfbd270b9b6b67028dd9bd566b3980ef62400bcbaff0c1c7c46697f9e6'
